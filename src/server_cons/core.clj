942843cf3e6533919e2b5b80ccd1c24d00ac1562
Nicolas Berger <nicoberger@gmail.com>
2014-11-15 09:10:15 -0300

    Add comment with result of examples

--- src/server_cons/core.clj
+++ src/server_cons/core.clj
@@ -70,7 +70,6 @@
 
   (let [machines []]
     (run 1 [q] (allocate-machines machines q)))
-
   (let [machines [{:id 1 :cpu-avg 22}]]
     (run 1 [q] (allocate-machines machines q)))
 
@@ -78,6 +77,7 @@
                   {:id 2 :cpu-avg 17}
                   ]]
     (run 1 [q] (allocate-machines machines q)))
+  ;; ((({:cpu-avg 22, :id 1} {:cpu-avg 17, :id 2})))
 
   (let [machines [{:id 1 :cpu-avg 22}
                   {:id 2 :cpu-avg 17}
@@ -87,6 +87,7 @@
                   {:id 6 :cpu-avg 11}
                   {:id 7 :cpu-avg 7}]]
     (run 1 [q] (allocate-machines machines q)))
+  ;; ((({:cpu-avg 22, :id 1} {:cpu-avg 17, :id 2} {:cpu-avg 3, :id 4} {:cpu-avg 6, :id 5} {:cpu-avg 11, :id 6}) ({:cpu-avg 7, :id 7} {:cpu-avg 22, :id 3})))
 
   (let [machines [{:id 1 :cpu-avg 22}
                   {:id 2 :cpu-avg 17}
@@ -99,5 +100,7 @@
                   {:id 9 :cpu-avg 29}
                   {:id 10 :cpu-avg 7}]]
     (run 1 [q] (allocate-machines machines q)))
+  ;; ((({:cpu-avg 22, :id 1} {:cpu-avg 17, :id 2} {:cpu-avg 3, :id 4} {:cpu-avg 6, :id 5} {:cpu-avg 11, :id 6}) ({:cpu-avg 7, :id 10} {:cpu-avg 29, :id 9} {:cpu-avg 22, :id 3}) ({:cpu-avg 35, :id 7}) ({:cpu-avg 26, :id 8})))
+
 
   )
