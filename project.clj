98eff208333cfefbf9002f3d4cba4854981fcbd7
Leandro Oliveira <git@buzzlabs.com.br>
2014-11-16 10:40:07 -0200

    midje

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject server-cons "0.1.0-SNAPSHOT"
+(defproject server-cons "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.logic "0.8.8"]])
+                 [org.clojure/core.logic "0.8.8"]
+                 [midje "1.6.3"]]
+  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
--- test/server_cons/core_test.clj
+++ test/server_cons/core_test.clj
@@ -1,7 +1,57 @@
 (ns server-cons.core-test
-  (:require [clojure.test :refer :all]
-            [server-cons.core :refer :all]))
+  (:require [midje.sweet :refer [=> throws fact facts future-fact]]
+            [clojure.core.logic :refer [run]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(require '[server-cons.core :refer [allocate-machines]])
+
+
+(facts "about server-cons"
+
+  (fact
+    (let [machines []]
+      (run 1 [q] (allocate-machines machines q))) => '([]))
+
+  (fact
+    (let [machines [{:id 1 :cpu-avg 22}]]
+      (run 1 [q] (allocate-machines machines q))) => `((({:id 1 :cpu-avg 22}))))
+
+  (fact
+    (let [machines [{:id 1 :cpu-avg 22}
+                    {:id 2 :cpu-avg 17}]]
+      (run 1 [q] (allocate-machines machines q)) => `((({:id 1 :cpu-avg 22}
+                                                        {:id 2 :cpu-avg 17})))))
+
+  (fact
+    (let [machines [{:id 1 :cpu-avg 22}
+                    {:id 2 :cpu-avg 17}
+                    {:id 3 :cpu-avg 22}
+                    {:id 4 :cpu-avg 3}
+                    {:id 5 :cpu-avg 6}
+                    {:id 6 :cpu-avg 11}
+                    {:id 7 :cpu-avg 7}]]
+      (run 1 [q] (allocate-machines machines q)))
+         => `((({:id 1 :cpu-avg 22}
+                {:id 2 :cpu-avg 17}
+                {:id 4 :cpu-avg 3}
+                {:id 5 :cpu-avg 6}
+                {:id 6 :cpu-avg 11})
+               ({:id 7 :cpu-avg 7}
+                {:id 3 :cpu-avg 22}))))
+  (future-fact
+
+    (let [machines [{:id 1 :cpu-avg 22}
+                    {:id 2 :cpu-avg 17}
+                    {:id 3 :cpu-avg 22}
+                    {:id 4 :cpu-avg 3}
+                    {:id 5 :cpu-avg 6}
+                    {:id 6 :cpu-avg 11}
+                    {:id 7 :cpu-avg 35}
+                    {:id 8 :cpu-avg 26}
+                    {:id 9 :cpu-avg 29}
+                    {:id 10 :cpu-avg 7}]]
+      (run 1 [q] (allocate-machines machines q)))
+         => `())
+
+
+
+  )
