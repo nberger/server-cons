6d5f4448241241a4039d51263ddb41ffd5ffbcf0
Nicolas Berger <nicoberger@gmail.com>
2014-11-24 02:48:11 -0300

    remove min-id optimization & some unused vars

--- src/server_cons/core.clj
+++ src/server_cons/core.clj
@@ -19,7 +19,7 @@
          (fd/>= remaining-cpu 0)))
 
 (defn machinesgroupo
-  ([all-machines machine-ids final-rest-ids min-id max-cpu group]
+  ([all-machines machine-ids final-rest-ids max-cpu group]
    (conda
      ;; no machines -> finish here
      [(emptyo machine-ids) (== machine-ids final-rest-ids) (emptyo group)]
@@ -33,29 +33,24 @@
         ;; branch 2: try to add a machine to the group
         [(fresh [id rest-group rest-ids remaining-cpu]
                 (rembero id machine-ids rest-ids)
-                (fd/> id min-id)
 
                 (enoughcpuo all-machines id max-cpu remaining-cpu)
 
                 (conso id rest-group group)
 
-                (machinesgroupo all-machines rest-ids final-rest-ids id remaining-cpu rest-group))])])))
+                (machinesgroupo all-machines rest-ids final-rest-ids remaining-cpu rest-group))])])))
 
 (defn make-groups4
   ([all-machines machine-ids max-cpu groups]
-   (make-groups4 all-machines 0 machine-ids max-cpu groups))
-
-  ([all-machines min-id machine-ids max-cpu groups]
    (conda
      [(emptyo machine-ids) (emptyo groups)]
-     [(fresh [group first-id rest-groups rest-ids permuted-ids next-ids]
+     [(fresh [group rest-groups rest-ids]
 
-             (machinesgroupo all-machines machine-ids rest-ids min-id max-cpu group)
+             (machinesgroupo all-machines machine-ids rest-ids max-cpu group)
 
              (!= group [])
              (conso group rest-groups groups)
-             (firsto group first-id)
-             (make-groups4 all-machines first-id rest-ids max-cpu rest-groups)
+             (make-groups4 all-machines rest-ids max-cpu rest-groups)
              )])))
 
 (defn- find-by-id
