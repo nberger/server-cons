821da89d0189b9670eeca4d922fea3fbfdacd3e3
Nicolas Berger <nicoberger@gmail.com>
2014-11-24 10:31:18 -0300

    core.logic 0.8.9

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.logic "0.8.8"]
+                 [org.clojure/core.logic "0.8.9"]
                  [midje "1.6.3"]
                  [org.clojure/test.check "0.6.0"]
                  [compojure "1.2.1"]
