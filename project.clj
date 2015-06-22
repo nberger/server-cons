b709c41718c0771fcff9b086d8d20e362d826502
Nicolas Berger <nicoberger@gmail.com>
2015-06-21 23:43:58 -0300

    Upgrade clojure, core.logic & test.check

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.logic "0.8.8"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
+                 [org.clojure/core.logic "0.8.10"]
                  [midje "1.6.3"]
-                 [org.clojure/test.check "0.6.0"]
+                 [org.clojure/test.check "0.7.0"]
                  [compojure "1.2.1"]
                  [ring/ring-core "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
