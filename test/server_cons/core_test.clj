327178845a8f7e7d7025594a77e3fa9b536c0217
Nicolas Berger <nicoberger@gmail.com>
2015-06-23 14:40:06 -0300

    Fix tests

--- test/server_cons/core_test.clj
+++ test/server_cons/core_test.clj
@@ -1,5 +1,5 @@
 (ns server-cons.core-test
-  (:require [server-cons.core :refer [allocate-machines]]
+  (:require [server-cons.core :refer [allocate-machines allocate-machines*]]
             [server-cons.generators :refer [machines-gen]]
             [clojure.core.logic :refer [run]]
             [clojure.test :refer [is testing deftest]]
@@ -44,6 +44,7 @@
       (is (= [[{:id 1 :cpu-avg 22}]]
              (allocate-machines machines)))))
 
+
   (testing "2 machines"
     (let [machines [{:id 1 :cpu-avg 22}
                     {:id 2 :cpu-avg 17}]]
@@ -58,15 +59,18 @@
                     {:id 4 :cpu-avg 3}
                     {:id 5 :cpu-avg 6}
                     {:id 6 :cpu-avg 11}
-                    {:id 7 :cpu-avg 7}]]
-      (is (= [[{:id 1 :cpu-avg 22}
-               {:id 2 :cpu-avg 17}
-               {:id 4 :cpu-avg 3}
-               {:id 5 :cpu-avg 6}
-               {:id 6 :cpu-avg 11}]
-              [{:id 7 :cpu-avg 7}
-               {:id 3 :cpu-avg 22}]]
-             (allocate-machines machines 60)))))
+                    {:id 7 :cpu-avg 7}]
+          result (allocate-machines* machines 60)]
+      (is (< 10 (count result)))
+      (is (contains? (set result)
+                     [[{:id 1 :cpu-avg 22}
+                       {:id 2 :cpu-avg 17}
+                       {:id 4 :cpu-avg 3}
+                       {:id 5 :cpu-avg 6}
+                       {:id 6 :cpu-avg 11}]
+                      [{:id 3 :cpu-avg 22}
+                       {:id 7 :cpu-avg 7}]]
+                     ))))
 
   (testing "10 machines"
 
@@ -80,14 +84,16 @@
                     {:id 8 :cpu-avg 26}
                     {:id 9 :cpu-avg 29}
                     {:id 10 :cpu-avg 7}]]
-      (is (= [[{:cpu-avg 22, :id 1}
-                {:cpu-avg 17, :id 2}
-                {:cpu-avg 3, :id 4}
-                {:cpu-avg 6, :id 5}
-               {:cpu-avg 11, :id 6}]
-               [{:cpu-avg 7, :id 10}
-                {:cpu-avg 29, :id 9}
-                {:cpu-avg 22, :id 3}]
-               [{:cpu-avg 35, :id 7}]
-               [{:cpu-avg 26, :id 8}]]
-             (allocate-machines machines))))))
+      (is (->> (allocate-machines* machines 60)
+               (filter (partial = [[{:cpu-avg 22, :id 1}
+                                    {:cpu-avg 17, :id 2}
+                                    {:cpu-avg 3, :id 4}
+                                    {:cpu-avg 6, :id 5}
+                                    {:cpu-avg 11, :id 6}]
+                                   [{:cpu-avg 22, :id 3}
+                                    {:cpu-avg 29, :id 9}
+                                    {:cpu-avg 7, :id 10}]
+                                   [{:cpu-avg 35, :id 7}]
+                                   [{:cpu-avg 26, :id 8}]]))
+               first
+               boolean)))))
