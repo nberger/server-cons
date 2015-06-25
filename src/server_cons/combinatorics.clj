b0892c73642dc0031acd0b5dca5186afdeb256f2
Nicolas Berger <nicoberger@gmail.com>
2015-06-25 12:55:32 -0300

    refactor combinatorics solution

--- src/server_cons/combinatorics.clj
+++ src/server_cons/combinatorics.clj
@@ -1,19 +1,13 @@
 (ns server-cons.combinatorics
   (:require [clojure.math.combinatorics :as combo]))
 
-(defn enoughcpu
-  [max-cpu [machine & more]]
-  (if machine
-    (let [cpu (:cpu-avg machine)]
-      (and
-        (<= cpu max-cpu)
-        (enoughcpu (- max-cpu cpu) more)))
-    true))
+(defn enoughcpu?
+  [max-cpu machines]
+  (>= max-cpu (reduce + :cpu-avg machines)))
 
 (defn all-groups-enough-cpu?
   [max-cpu groups]
-  (every? (partial enoughcpu max-cpu) groups)
-  )
+  (every? (partial enoughcpu? max-cpu) groups))
 
 (defn allocate-by-partitions
   [max-cpu machines]
